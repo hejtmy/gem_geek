23eaff712e72e70a2bed3276195c3fce2cf6fc7d
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-01-10 23:08:22 +0000

    Adds more functions

--- lib/gem_geek.rb
+++ lib/gem_geek.rb
@@ -3,6 +3,18 @@ module GemGeek
 	def self.get_user(username, options = {})
 		BGGGetter.get_user(username, options)
 	end
+	def self.get_family(id, options = {})
+		BGGGetter.get_family(id, options)
+	end
+
+	def self.get_collection(username, options = {})
+		BGGGetter.get_collection(username, options)
+	end
+	
+	def self.get_item(id, statistics = false, api = 2, options = {})
+			BGGGetter.get_collection(id, statistics, api, options)
+	end
+	
 end
 
 require 'gem_geek/bgg_getter'

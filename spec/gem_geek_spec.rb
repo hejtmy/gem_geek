908fc77d3ba53eb9110e8e1eb15b2a79b3eafc41
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-01-11 21:57:37 +0000

    Adds rspec tests

--- spec/gem_geek_spec.rb
+++ spec/gem_geek_spec.rb
@@ -1,15 +1,27 @@
 require 'spec_helper'
 
+USERNAME = 'Tatsukochi'
+
 describe GemGeek do
   it 'has a version number' do
     expect(GemGeek::VERSION).not_to be nil
   end
   
   it 'downloads user data appropriately' do
-    expect(GemGeek.get_user('Tatsukochi')).not_to be nil
+    expect(GemGeek.get_user(USERNAME)).not_to be nil
   end
   
   it 'returns null in case nothing is found' do
     expect(GemGeek.get_user('aksjhdoajsdkljqwhohasdalsh')).to be nil
+  end
+  
+  it "download games without issues" do
+    plays = GemGeek.get_plays(USERNAME)
+    plays.plays.count >= 200
+  end
+  
+  it "doesn't input game if it already exists" do
+    plays = GemGeek.get_plays(USERNAME)
+    expect(plays.add(plays.plays[1])).to be false
   end
 end

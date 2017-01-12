fc88ed10fb4c2364ee42638bbd7dfe5de1f34ecd
hejtmy <hejtmy@gmail.com>
2017-01-12 14:52:31 +0100

    Adds date tests as well as chaining tests

--- spec/gem_geek_spec.rb
+++ spec/gem_geek_spec.rb
@@ -25,4 +25,13 @@ describe GemGeek do
   it "doesn't input game if it already exists" do
     expect(plays.add(plays.plays[1])).to be false
   end
+
+  it "appropriately selects based on date" do
+    expect(plays.after_date(Date.today.next_day).empty?).to be true
+    expect(plays.before_date(Date.today).empty?).to be false
+  end
+
+  it "allows chaining of commands" do
+    expect(plays.before_date(Date.today).game('Takenoko').empty?).to be false
+  end
 end

13e85529b458b4810437e646500901fa3eaf2ec7
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-01-15 21:56:50 +0000

    Adds fullname information

--- lib/gem_geek/data_types/bgg_user.rb
+++ lib/gem_geek/data_types/bgg_user.rb
@@ -3,14 +3,14 @@
 module GemGeek
 	class BGGUser < BGGBase
 
-		attr_reader :id, :name, :avatar, :year_registered, :last_login, :state, :trade_rating
+		attr_reader :id, :name, :avatar, :year_registered, :last_login, :state, :trade_rating, :first_name, :last_name
 
 		def initialize(xml)
 			if !xml.nil?
 				@id = get_integer(xml, "user", "id")
 				@name = get_string(xml, "user", "name")
 				@first_name = get_string(xml, "firstname", "value")
-				@last_name = get_string(xml, "lastname", "lastname")
+				@last_name = get_string(xml, "lastname", "value")
 				@avatar = get_string(xml, "avatarlink", "value")
 				@year_registered = get_integer(xml, "yearregistered", "value")
 				@last_login = get_string(xml, "lastlogin", "value")
@@ -18,6 +18,8 @@ module GemGeek
 				@trade_rating = get_integer(xml, "traderating", "value")
 			else
 				@id = -1
+				@first_name = ""
+				@last_name = ""
 				@name = ""
 				@avatar = ""
 				@year_registered = -1
@@ -26,7 +28,13 @@ module GemGeek
 				@trade_rating = -1
 			end
 		end
-
+		
+		def full_name
+			if not (self.first_name.empty? && self.last_name.empty?)
+				self.first_name + " " + self.last_name
+			end
+		end
+		
 		def get_collection
 			return BGGGetter.get_collection(@name)
 		end

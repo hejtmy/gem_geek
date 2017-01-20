ebdd1f3a145cd25e6e3c8962e65a4c1076730f67
Lukáš Hejtmánek <hejtmy@gmail.com>
2017-01-20 15:50:30 +0000

    Initial info class

---
+++ lib/gem_geek/info/bgg_info_plays.rb
@@ -0,0 +1,16 @@
+module GemGeek
+    class BGGInfoPlays
+        attr_reader :plays
+        
+        def initialize(plays = nil)
+            @plays = plays
+        end
+        
+        def analyse(game_id)
+            
+        end
+        
+        private
+        
+    end
+end
